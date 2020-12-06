6e6fd063f878d3f611fc959c2ef7f8bd662ab19e
Windows_PC\marxc <marxc9@outlook.de>
2020-12-06 23:59:41 +0100

    using arduino's internal pull-up resistors --- fixing default hotkeys

--- arduino/streamdeck_sketch/streamdeck_sketch.ino
+++ arduino/streamdeck_sketch/streamdeck_sketch.ino
@@ -1,16 +1,34 @@
 #define MAGIC_WORDS       "ccstreamdeck" // first words that are send so the pc knows that this is the stream deck
 
 #define BAUD_RATE         9600  // baud rate for serial communication
-#define NUM_BUTTONS       6     // number of physical buttons connected 
-#define BUTTON_START_PIN  2     // first pin of first button, second button needs to be connected to BUTTON_START_PIN+1, etc.
+#define NUM_BUTTONS       15     // number of physical buttons connected 
 #define GREEN_LED_PIN     12    // IO pin of green led
 #define RED_LED_PIN       13    // IO pin of red led
 
 #define BUTTON_TRIGGER_COOLDOWN 100 // (ms) how long until trigger event can be sent again
 #define CONNECTION_LOST_TIMEOUT 500 // (ms) how long until connection is lost if no
 
-#define PULL_UP_RESISTOR  // if PULL_UP_RESISTOR is defined, buttons connect input pins to GROUND when pressed,
-                          // if PULL_UP_RESISTOR is NOT defined, buttons connect input pins to VDD when pressed
+#define PULL_UP_RESISTOR  // if PULL_UP_RESISTOR is defined, buttons connect input pins to GROUND when pressed, the internal pull up resistors of the arduino are used
+                          // if PULL_UP_RESISTOR is NOT defined, buttons connect input pins to VDD when pressed, external pull down resistors have to be connected to the arduino
+
+// assign gpio pin to each button
+int BUTTON_PINS[NUM_BUTTONS] = {
+  2, // button 1
+  3, // button 2
+  4, // button 3
+  5, // button 4
+  6, // button 5
+  7, // button 6
+  8, // button 7
+  9, // button 8
+  10,// button 9
+  11,// button 10
+  14,// button 11
+  15,// button 12
+  16,// button 13
+  17,// button 14
+  18 // button 15
+};
 
 // array keeping track of button states (1: pressed, 0: released)
 int BUTTON_STATES[NUM_BUTTONS];
@@ -30,8 +48,12 @@ void setup() {
   // set button pins as input
   for(int i = 0; i < NUM_BUTTONS; i++){
     BUTTON_STATES[i] = 0;
-    LAST_BUTTON_TRIGGER_TIME[i] = 0;   
-    pinMode(BUTTON_START_PIN+i, INPUT);
+    LAST_BUTTON_TRIGGER_TIME[i] = 0;
+    #ifdef PULL_UP_RESISTOR
+      pinMode(BUTTON_PINS[i], INPUT_PULLUP);
+    #else
+      pinMode(BUTTON_PINS[i], INPUT);
+    #endif
   }
 
   // set led pins as output
@@ -71,7 +93,7 @@ void loop() {
     // check buttons pressed
     for(byte i = 0; i < NUM_BUTTONS; i++){
       int btn_state_before = BUTTON_STATES[i];
-      BUTTON_STATES[i] = digitalRead(BUTTON_START_PIN+i);
+      BUTTON_STATES[i] = digitalRead(BUTTON_PINS[i]);
       #ifdef PULL_UP_RESISTOR
         BUTTON_STATES[i] = 1-BUTTON_STATES[i];
       #endif
@@ -92,6 +114,7 @@ void loop() {
     }
     if(millis() - LAST_ACTIVE_RECEIVED >= CONNECTION_LOST_TIMEOUT){
       CONNECTED = false;
+      digitalWrite(GREEN_LED_PIN, LOW);
     }
     delay(1); 
   }
